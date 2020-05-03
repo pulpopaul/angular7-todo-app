aa4946a9387cacd81ae990fc09d3bc923ff74d3d
Pablo G <pulpopaul00@gmail.com>
2020-05-03 18:33:36 -0300

    express - nodemon added

--- express-api/index.js
+++ express-api/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
 
 // Connect to MongoDB
 mongoose
@@ -17,20 +20,8 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const Item = require('./models/Item');
-
 app.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
-});
-
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
-    name: req.body.name
-  });
-
-  newItem.save().then(item => res.redirect('/'));
+  res.json({ "message": "Welcome to the todo api." });
 });
 
 const port = 3000;
